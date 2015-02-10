de734419c3e3113ec31ab21617087f3e91becf6a
Alex Wu <aw246@st-andrews.ac.uk>
2015-02-10 16:02:27 +0000

    make more object oriented

--- robotics/skynet/src/skynet/Obstacle.java
+++ robotics/skynet/src/skynet/Obstacle.java
@@ -1,60 +1,54 @@
 package skynet;
 
+import java.util.ArrayList;
+import java.util.Random;
+import easyGui.EasyGui;
 import renderables.*;
 import geometry.IntPoint;
 
-public class Obstacle {
-	private RenderableOval ob;
-	private int xCenter,yCenter,radius;
+public class Obstacle extends Object{
+	private RenderableOval obst;
 	
 	Obstacle(int cX, int cY, int r){
-		ob = new RenderableOval(cX,cY,2*r,2*r);
-		xCenter=cX;
-		yCenter=cY;
-		radius=r;
-	}
-	
-	public boolean isEmpty(){
-		return ob==null;
-	}
-	
-	RenderableOval getObstacle(){
-		return ob;
-	}
-	
-	public int getX(){
-		return xCenter;
-	}
-	
-	public int getY(){
-		return yCenter;
-	}
-	
-	public int getR(){
-		return radius;
+		super(cX,cY,r);
+		obst = new RenderableOval(cX,cY,2*r,2*r);
 	}
 	
 	public RenderableOval getRenderable(){
-		return ob;
+		return obst;
 	}
-	
-	//for point intersecting with obstacle
-	public boolean didCollide(IntPoint p){
-		int distX=xCenter-p.x;
-		int distY=yCenter-p.y;
-		if(dist(distX,distY)<=radius) return true;
-		else return false;
+
+	//generate random obstacles on a gui field
+	public ArrayList<Obstacle> initObstacles(Object start, Object goal, int xPixels,int yPixels,EasyGui gui){
+		int x,y,r;
+		Obstacle tmp;
+		ArrayList<Obstacle> obstacles = new ArrayList<Obstacle>();;
+		
+		// New Random generator
+		Random randGen = new Random();
+		
+		// Number of obstacles
+		int num = randGen.nextInt(11);
+		
+		
+		for(int i=0;i<num;i++){
+			x=randGen.nextInt(xPixels+1);
+			y=randGen.nextInt(yPixels+1);
+			r=randGen.nextInt(getRadius()+1);
+			tmp = new Obstacle(x,y,r);
+			// If obstacles intersects with the start or goal
+			if(tmp.didCollide(start) || tmp.didCollide(goal)){
+				i--;
+				tmp=null;
+				continue;
+			}
+			else{
+				obstacles.add(tmp);
+				gui.draw(tmp.getRenderable());
+			}
+		}
+		return obstacles;
 	}
-	
-	//for circle intersecting with obstacle
-	public boolean didIntersect(int x,int y,int r){
-		int distX=xCenter-x;
-		int distY=yCenter-y;
-		if(dist(distX,distY)<=r+radius) return true;
-		else return false;
-	}
-	
-	public double dist(int distX, int distY){
-		return Math.sqrt(distX*distX+distY*distY);
-	}
+
+
 }
